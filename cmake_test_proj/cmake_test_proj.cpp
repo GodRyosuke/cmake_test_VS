3b347a8b3b026c357e2a2f98a2b749dad86d5fd7
GodRyosuke <ienvs0828@outlook.jp>
2021-12-29 13:58:13 +0900

    Testを追加した

--- cmake_test_proj/cmake_test_proj.cpp
+++ cmake_test_proj/cmake_test_proj.cpp
@@ -3,12 +3,40 @@
 
 #include "cmake_test_proj.h"
 #include <string>
+#include <stdlib.h>
 
-
-int main()
+int main(int argc, char** argv)
 {
-	std::string str = "output text";
-	std::cout << str << std::endl;
-	std::cout << "Hello CMake. from wander!" << std::endl;
-	return 0;
+    if (argc == 2) {
+        std::string options = argv[1];
+        if (options == "-v") {
+            FILE* fp;
+            char c;
+            // resourcesから読み出していることに注意!
+            if ((fp = fopen("../resources/about.txt", "r")) == NULL) { // configファイルの読み出し
+                printf("file open error!!\n");
+                return 1;
+            }
+            while ((c = fgetc(fp)) != EOF) {
+                putchar(c);
+            }
+            fclose(fp);
+        }
+    }
+
+
+    FILE* fp;  /* ファイルポインタの宣言 */
+    char c;
+    if ((fp = fopen("../resources/hello_cmake.txt", "r")) == NULL) {  /* ファイルのオープン */
+        printf("file open error!!\n");
+        return 1;
+    }
+    /* fgetc()で取得 */
+    //printf("Read by fgetc() ----------------------------------------\n");
+    while ((c = fgetc(fp)) != EOF) {
+        putchar(c);
+    }
+    fclose(fp);
+
+    return 0;
 }
