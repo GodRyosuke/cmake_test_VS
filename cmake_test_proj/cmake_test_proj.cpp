23a73c1fc3b81b0e5fdba186134cca12c43b3088
GodRyosuke <ienvs0828@outlook.jp>
2021-12-29 11:26:48 +0900

    initial commit

---
+++ cmake_test_proj/cmake_test_proj.cpp
@@ -0,0 +1,12 @@
+﻿// cmake_test_proj.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "cmake_test_proj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. from wander!" << endl;
+	return 0;
+}
